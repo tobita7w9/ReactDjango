f56f0ac9a70e2f608bb5dc0645f9d8a188cc79eb
TobitaKosuke <ktobita7w9@gmail.com>
2022-10-01 22:13:45 +0900

    サンプルモデルの作成

--- django_server/api_server/models.py
+++ django_server/api_server/models.py
@@ -4,5 +4,6 @@ class SampleModel(models.Model):
     title = models.CharField(max_length = 100)
     description = models.CharField(max_length = 300)
 
+
     def __str__(self):
         return self.title
